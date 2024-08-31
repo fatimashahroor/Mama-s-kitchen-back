4a1da88aaff6b4439a39223e53a5632ba41303cb
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-08-31 17:03:42 +0300

    Added routes for user and dish controllers

--- my-kitchen/routes/api.php
+++ my-kitchen/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\DishController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Spatie\Permission\Models\Role;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +16,15 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles','RoleController');
     Route::resource('users','UserController');
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/role/{id}', [RoleController::class, 'show']);
+Route::post('/role/create', [RoleController::class, 'store']);
+Route::put('/role/update/{id}', [RoleController::class, 'update']);
+
+Route::get('/dish/{id}', [DishController::class, 'show']);
+Route::post('/dish/create', [DishController::class, 'store']);
+Route::post('/dish/update/{id}', [DishController::class, 'update']);
+Route::delete('/dish/delete/{id}', [DishController::class, 'destroy']);
