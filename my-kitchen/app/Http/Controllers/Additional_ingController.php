eedd8d35c19bd36fe853d4e4c28d0adaef6edb36
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-01 01:49:54 +0300

    Implemented api show all and function create additional ingredient to be called in the create ingredient api

--- my-kitchen/app/Http/Controllers/Additional_ingController.php
+++ my-kitchen/app/Http/Controllers/Additional_ingController.php
@@ -3,14 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Additional_ing;
+use Illuminate\Support\Facades\Validator;
 
 class Additional_ingController extends Controller
 {
     function __construct()
     {
-         $this->middleware('permission:additional_ing-list');
-         $this->middleware('permission:additional_ing-create', ['only' => ['create','store']]);
-         $this->middleware('permission:additional_ing-edit', ['only' => ['edit','update']]);
-         $this->middleware('permission:additional_ing-delete', ['only' => ['destroy']]);
+        //  $this->middleware('permission:additional_ing-list');
+        //  $this->middleware('permission:additional_ing-edit', ['only' => ['edit','update']]);
+        //  $this->middleware('permission:additional_ing-delete', ['only' => ['destroy']]);
+    }
+
+    public function index()
+    {
+        $additional_ings= Additional_ing::latest()->paginate(5);
+        if ($additional_ings['total'] == 0) {
+            return response()->json(['message' => 'No additional ingredients found'], 404);
+        }
+        return response()->json($additional_ings);
+    }
+
+    public static function store($user_id, $ingredient_id, $cost)
+    {
+        $additional_ing = new Additional_ing();
+        $additional_ing->user_id = $user_id;
+        $additional_ing->ingredient_id = $ingredient_id;
+        $additional_ing->cost = $cost;
+        if (!$additional_ing->save()) {
+            return ['message' => 'Error while creating additional ingredient'];
+        }
+        return ['message' => 'Additional ingredient created successfully', 'additional_ing' => $additional_ing];
     }
 }
