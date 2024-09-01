7184785e57ae76caf94dc0e8fa1a5af937e7bab1
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-01 03:24:16 +0300

    Created the apis for deleting and updating additional ingredient

--- my-kitchen/app/Http/Controllers/Additional_ingController.php
+++ my-kitchen/app/Http/Controllers/Additional_ingController.php
@@ -10,9 +10,9 @@ class Additional_ingController extends Controller
 {
     function __construct()
     {
-        //  $this->middleware('permission:additional_ing-list');
-        //  $this->middleware('permission:additional_ing-edit', ['only' => ['edit','update']]);
-        //  $this->middleware('permission:additional_ing-delete', ['only' => ['destroy']]);
+         $this->middleware('permission:additional_ing-list');
+         $this->middleware('permission:additional_ing-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:additional_ing-delete', ['only' => ['destroy']]);
     }
 
     public function index()
@@ -34,5 +34,39 @@ class Additional_ingController extends Controller
             return ['message' => 'Error while creating additional ingredient'];
         }
         return ['message' => 'Additional ingredient created successfully', 'additional_ing' => $additional_ing];
+    }
+
+    public function update ($id, Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'cost' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $additional_ing = Additional_ing::find($id);
+        if (!$additional_ing) {
+            return response()->json(['message' => 'Additional ingredient not found'], 404);
+        }
+        $additional_ing->update($request->all());
+        return response()->json(['message' => 'Additional ingredient updated successfully', 'additional_ing' => $additional_ing], 200);
+    }
+    public function show($id)
+    {
+        $additional_ing= Additional_ing::find($id);
+        if (!$additional_ing) {
+            return response()->json(['message' => 'Additional ingredient not found'], 404);
+        }
+        return response()->json($additional_ing);
+    }
+
+    public function destroy($id)
+    {
+        $additional_ing = Additional_ing::find($id);
+        if (!$additional_ing) {
+            return response()->json(['message' => 'Additional ingredient not found'], 404);
+        }
+        $additional_ing->delete();
+        return response()->json(['message' => 'Additional ingredient deleted successfully']);
     }
 }
