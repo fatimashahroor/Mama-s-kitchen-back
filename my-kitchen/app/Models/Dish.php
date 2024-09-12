d520a32cb7358beea88a78bca4b0fe90ba099b85
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-13 00:41:31 +0300

    Added the fillable columns

--- my-kitchen/app/Models/Dish.php
+++ my-kitchen/app/Models/Dish.php
@@ -9,7 +9,7 @@ class Dish extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'name', 'steps', 'price', 'image_path', 'available_on'];
+    protected $fillable = ['user_id', 'name', 'steps', 'price', 'image_path', 'available_on', 'duration', 'diet_type', 'main_ingredients'];
 
     public function user()
     {
@@ -21,11 +21,6 @@ class Dish extends Model
         return $this->hasMany(Review::class);
     }
 
-    public function ingredients()
-    {
-        return $this->belongsToMany(Ingredient::class);
-    }
-
     public function orders()
     {
         return $this->belongsToMany(Order::class);
@@ -33,6 +28,6 @@ class Dish extends Model
 
     public function additional_ingredients()
     {
-        return $this->belongsToMany(Additional_ing::class);
+        return $this->belongsToMany(Additional_ing::class, 'dishes_additional_ings', 'dish_id', 'additional_ing_id');
     }
 }
