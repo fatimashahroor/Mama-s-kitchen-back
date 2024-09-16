8c3214247ea4e346b5b81e59fba87bd292885776
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-16 14:09:51 +0300

    Added a column to a=categorize orders according to cook_id

--- my-kitchen/database/migrations/2024_08_22_213516_create_orders_table.php
+++ my-kitchen/database/migrations/2024_08_22_213516_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('cook_id');
             $table->foreignId('location_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->string('status');
             $table->dateTime('order_date');
